b64b958c200f1bed4b381c011472cf529d5fd456
Rayni Nunez Espino <timetopwm@gmail.com>
2024-11-24 16:16:25 -0400

    Saving nvim configuration.

---
+++ lua/configs/conform.lua
@@ -0,0 +1,34 @@
+local options = {
+    formatters_by_ft = {
+        lua = { "stylua" },
+        go = { "gofumpt", "goimports-reviser", "golines" },
+        python = { "isort", "black" },
+        -- css = { "prettier" },
+        -- html = { "prettier" },
+    },
+    formatters = {
+        -- Python
+        black = {
+            prepend_args = {
+                "--fast",
+                "--line-length",
+                "80",
+            },
+        },
+        isort = {
+            prepend_args = {
+                "--profile",
+                "black",
+            },
+        },
+    },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+}
+
+require("conform").setup(options)
+
+-- return options
